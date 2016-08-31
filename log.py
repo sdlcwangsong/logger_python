808bfdcea7b6042d58900f529173d39d4ddf96b2
songwang11 <songwang11@creditease.cn>
2016-08-31 12:02:53 +0800

    modify log

--- log.py
+++ log.py
@@ -3,22 +3,15 @@ import logging
 import logging.config
 
 class Log(object):
-    def __init__(self, name):
-        logging.config.fileConfig("logger.conf")
+    def __init__(self, name, conf):
+        logging.config.fileConfig(conf)
         self.logger = logging.getLogger(name)
-    
-    def debug(self, message):
-        return self.logger.debug(message)
-        
-    def info(self, message):
-        return self.logger.info(message)
-        
-    def warning(self, message):
-        return self.logger.warning(message)
 
+    
+    
 if __name__=='__main__':   
     
-    logger = Log("sWang")
+    logger = Log("sWang","logger.conf").logger
     logger.debug('This is debug message')
     logger.info('This is info message')
     logger.warning('This is warning message')
